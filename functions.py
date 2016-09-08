98a04ec13c5bd8b52eb7b8043e1636ab73673be8
Micah Picasso <mpicasso@localhost.localdomain>
2016-09-08 16:48:56 -0600

    Finished Lower and upper functions and implemented in main

--- functions.py
+++ functions.py
@@ -1,16 +1,32 @@
+#TODO Ask if we need to preserve file extensions
 import sys
-print(sys.argv)
-#
+
+def checkYes(a):
+    return a in ('yes','y')
 
 def verbose():
     print("verbose")
 def trim():
     print("trim")
-def lower():
-    print("lower")
-def upper():
-    print("upper")
-def print():
+def lower(filename, interactive):
+    good = True
+    if interactive:
+        qStr = 'do you want to LowerCase ' + filename + '?(y/n): '
+        good = checkYes( input(qStr) )
+    if good:
+        return filename.lower()
+    else:
+        return filename
+def upper(filename, interactive):
+    good = True
+    if interactive:
+        qStr = 'do you want to UpperCase ' + filename + '?(y/n): '
+        good = checkYes( input(qStr) )
+    if good:
+        return filename.upper()
+    else:
+        return filename
+def oldNew():
     print("print")
 def replace():
     print("replace")
--- main.py
+++ main.py
@@ -7,49 +7,64 @@
 # Prints x**y and [n1,n2,...]
 
 import sys, argparse, glob, os, fnmatch
+from functions import lower,upper
 
 
-parser = argparse.ArgumentParser( usage = "-h for help, -v for verbose, -i for int, -f for float" )
-
-# optional switches (may occur in any order)
-parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
-parser.add_argument("-t", "--trim", type=int, metavar="N", help="positive n: trim n chars from the start of each filename negative n: trim n chars from the end of each filename")
-#parser.add_argument("-f", "--real", type=float, metavar="N", help="supply a float value")
-parser.add_argument("-l", "--lower", action="store_true", help="converts filename to lowercase")
-parser.add_argument("-u", "--uppper", action="store_true", help="converts filename to uppercase")
-parser.add_argument("-p", "--print", action="store_true", help="prints the old and new filenames without rewriting them")
-parser.add_argument("-i", "--interactive", action="store_true", help="turns on interactive mode")
-parser.add_argument("-r", "--replace", type=str, nargs=2, metavar=("oldfilename", "newfilename"), help="replaces the oldfilename with the new filename")
-parser.add_argument("-d", "--delete", action="store_true", help="deletes the file")
-parser.add_argument("-dt", "--touch", action="store_true", help="updates time stamps to current time")
-parser.add_argument("-D", "--date", type=int, metavar="DDMMYYYY", help="updates the date time stamps to specified date")
-parser.add_argument("-T", "--time", type=int, metavar="HHMMSS", help="updates the time stamps to specified date")
-
-
-
-# required positional arguments
-'''
-parser.add_argument("x", type=int, help="base")
-parser.add_argument("y", type=int, help="exponent")
-'''
-# followed by 0 or more strings
-parser.add_argument("names", type=str, nargs='+', help="list of strings")
-
-# parse command arguments
-args = parser.parse_args()
-
-# print results of parsing
-
-#print( args.x, "**", args.y, "=", args.x ** args.y )
-print( 'cmd args:', sys.argv )
-print( 'argparse:', args )
-print( 'args.verbose =', args.verbose )
-print( 'args.lower =', args.lower )
-print( "names: ", args.names )
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser( usage = "-h for help, -v for verbose, -i for int, -f for float" )
+
+    # optional switches (may occur in any order)
+    parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
+    parser.add_argument("-t", "--trim", type=int, metavar="N", help="positive n: trim n chars from the start of each filename negative n: trim n chars from the end of each filename")
+    #parser.add_argument("-f", "--real", type=float, metavar="N", help="supply a float value")
+    parser.add_argument("-l", "--lower", action="store_true", help="converts filename to lowercase")
+    parser.add_argument("-u", "--upper", action="store_true", help="converts filename to uppercase")
+    parser.add_argument("-p", "--print", action="store_true", help="prints the old and new filenames without rewriting them")
+    parser.add_argument("-i", "--interactive", action="store_true", help="turns on interactive mode")
+    parser.add_argument("-r", "--replace", type=str, nargs=2, metavar=("oldfilename", "newfilename"), help="replaces the oldfilename with the new filename")
+    parser.add_argument("-d", "--delete", action="store_true", help="deletes the file")
+    parser.add_argument("-dt", "--touch", action="store_true", help="updates time stamps to current time")
+    parser.add_argument("-D", "--date", type=int, metavar="DDMMYYYY", help="updates the date time stamps to specified date")
+    parser.add_argument("-T", "--time", type=int, metavar="HHMMSS", help="updates the time stamps to specified date")
+
+
+
+    # required positional arguments
+    '''
+    parser.add_argument("x", type=int, help="base")
+    parser.add_argument("y", type=int, help="exponent")
+    '''
+    # followed by 0 or more strings
+    parser.add_argument("names", type=str, nargs='+', help="list of strings")
+
+    # parse command arguments
+    args = parser.parse_args()
+
+    # print results of parsing
+
+    #print( args.x, "**", args.y, "=", args.x ** args.y )
+    '''print( 'cmd args:', sys.argv )
+    print( 'argparse:', args )
+    print( 'args.verbose =', args.verbose )
+    print( 'args.lower =', args.lower )
+    print( "names: ", args.names )
+    '''
+    
     #print("test")
+    originalFileNames = [];
     for s in args.names:
         for filename in glob.glob(s):
-            print(filename, end=" ")
-
+            originalFileNames.append(filename)
+    #print(originalFileNames)
+    newFileNames = originalFileNames[:]
+    
+    if args.lower:
+        for i in range(len(newFileNames)):
+            newFileNames[i] = lower(newFileNames[i], args.interactive)
+    if args.upper:
+        for i in range(len(newFileNames)):
+            newFileNames[i] = upper(newFileNames[i], args.interactive)
+    
+    print(newFileNames);

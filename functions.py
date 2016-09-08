d2190f17113b3e6761f2835e67562a51bcb95eb5
markb <self@localhost.localdomain>
2016-09-08 14:56:02 -0600

    add empty functions

--- functions.py
+++ functions.py
@@ -2,3 +2,22 @@ import sys
 print(sys.argv)
 #
 
+def verbose()
+
+def trim()
+
+def lower()
+
+def upper()
+
+def print()
+
+def replace()
+
+def delete()
+
+def touch()
+
+def date()
+
+def time()

ddde781a8b349aad500f7d4141e5dbd1a04df680
Micah Picasso <mpicasso@localhost.localdomain>
2016-09-21 20:12:14 -0600

    added comments to rename.py

--- rename.py
+++ rename.py
@@ -3,22 +3,23 @@
 # ***** cmdargparse.py *****
 # Python script to parse command-line arguments using argparse module.
 # CSC461 Programming Languages, Fall 2016 (JMW)
-# Usage: python cmdargparse.py [-h] [-v] [-i N] [-f N] x y [n1 n2 n3 ...]
-# Prints x**y and [n1,n2,...]
+# Authors: Mark Buttenhoff and Micah Picasso
+# Usage: python3 rename.py [-l] [-u] [-t N] [-r oldstring newstring] [-n count string] [-h] [-v] [-p] [-i] [-d] [-dt] [-D DDMMYYYY] [-T HHMMSS] file1 file2 ...
+# Modified from cmdargparse.py provided by Dr. Weiss for CSC461 Fall 2016
+
 
 import sys, argparse, glob, os, fnmatch
 from functions import *
 
 
-
+#used to simulate main
 if __name__ == '__main__':
-
+    #parser for parsing the given arguments
     parser = argparse.ArgumentParser( usage = "-h for help, -v for verbose, -i for int, -f for float" )
 
     # optional switches (may occur in any order)
     parser.add_argument("-v", "--verbose", action="store_true", help="verbose output")
     parser.add_argument("-t", "--trim", type=int,metavar="N", help="positive n: trim n chars from the start of each filename negative n: trim n chars from the end of each filename")
-    #parser.add_argument("-f", "--real", type=float, metavar="N", help="supply a float value")
     parser.add_argument("-l", "--lower", action="store_true", help="converts filename to lowercase")
     parser.add_argument("-u", "--upper", action="store_true", help="converts filename to uppercase")
     parser.add_argument("-p", "--print", action="store_true", help="prints the old and new filenames without rewriting them")
@@ -31,38 +32,31 @@ if __name__ == '__main__':
     parser.add_argument("-T", "--time", type=int, metavar="HHMMSS", help="updates the time stamps to specified date")
 
 
-
-    # required positional arguments
-    '''
-    parser.add_argument("x", type=int, help="base")
-    parser.add_argument("y", type=int, help="exponent")
-    '''
     # followed by 0 or more strings
     parser.add_argument("names", type=str, nargs='+', help="list of strings")
 
     # parse command arguments
     args = parser.parse_args()
-    # print results of parsing
-    #print( args.x, "**", args.y, "=", args.x ** args.y )
-    '''print( 'cmd args:', sys.argv )
-    print( 'argparse:', args )
-    print( 'args.verbose =', args.verbose )
-    print( 'args.lower =', args.lower )
-    print( "names: ", args.names )
-    '''
+
     
-    #print("test")
+    #make empty list to store file names
     originalFileNames = [];
+    
+    #get all the file names into the originalFilesNames list
     for s in args.names:
         for filename in glob.glob(s):
             originalFileNames.append(filename)
-    #print(originalFileNames)
+
+    #copy file names to new list
     newFileNames = originalFileNames[:]
     
+    #if delete is called all other ooptions are ignored
     if args.delete:
         delete(originalFileNames,args.interactive)
+    #all other arguments
     else:
         curIndex = 0;
+        #check each argument
         for curArg in sys.argv:
             if curArg in ('-l','--lower'):
                 for i in range(len(newFileNames)):
@@ -115,9 +109,10 @@ if __name__ == '__main__':
                 time(originalFileNames,args.time,args.interactive)
             
             curIndex += 1
-
+        #print new file names instead of renaming
         if args.print:
             oldNew(originalFileNames, newFileNames)
+        #rename files
         else:
             changename(originalFileNames, newFileNames)
 
